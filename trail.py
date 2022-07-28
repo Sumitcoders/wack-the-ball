93ca04f35927638461db0e653abf9f8bc62341d1
Sumitcoders <90320919+Sumitcoders@users.noreply.github.com>
2022-07-28 19:36:44 +0530

    the first update

--- trail.py
+++ trail.py
@@ -1,8 +1,11 @@
+import time
 import turtle
+import random
+
 
 window = turtle
 
-
+window.tracer(1,1)
 
 
 #shapes
@@ -17,10 +20,78 @@ ball = window.Turtle()
 ball.shape('ball.gif')
 ball.color('#fff')
 ball.direction = 'stop'
-ball.speed = 10
+ball.speed = 100
 ball.penup()
 ball.a = True
-ball.hide_status = 'no'
+ball.status = 'yes'
+ball.invinsible = 'no'
+
+
+
+
+txt = window.Turtle()
+txt.color('white')
+txt.hideturtle()
+
+b = window.Turtle()
+b.shape('square')
+b.color('#fff')
+b.penup()
+b.speed(100)
+b.goto(650,0)
+b.l = 10
+b.w = 0.5
+
+#invinsiblity seed
+###################################################################################################
+
+seed = window.Turtle()
+seed.shape('square')
+seed.color('#f90233')
+#seed.hideturtle()
+seed.penup()
+seed.speed(1000)
+seed.shapesize(1,1)
+seed.goto(200,200)
+seed.eaten = 'yes'
+def write(msg):
+    txt.write(msg,align='center',font=('Mono space', 14, 'italic'))
+    window.ontimer(txt.clear,3000)
+
+def summon_seed():
+    x = random.randint(-6,6)
+    y = random.randint(-3,3)
+    x = x*100
+    y = y*100
+    seed.goto(x,y)
+    seed.showturtle()
+    seed.eaten = 'no'
+def invinsiblity_over():
+    ball.invinsible = 'no'
+    write('Invinsiblity over')
+def seed_collosion():
+    if ball.xcor() + 2 > seed.xcor() - 10 and ball.xcor() - 2 < seed.xcor() + 10:
+        if ball.ycor() + 2 >seed.ycor() - 10 and ball.ycor() - 2 < seed.ycor() + 10:
+            print('incin')
+            seed.eaten = 'yes'
+            seed.goto(1000,1000)
+            ball.invinsible = 'yes'
+            write('The ball is now invinsible')
+            window.ontimer(invinsiblity_over,10000)
+            #summon_seed()
+
+
+
+
+
+
+
+
+
+
+
+####################################################################################################
+
 #functions
 
 def up():
@@ -33,24 +104,57 @@ def right():
     ball.direction = 'right'
 def stop():
     ball.direction = 'stop'
+def time_over():
+    ball.status = 'no'
+def size():
+    if b.l > 4:
+        b.l -= 0.01
+        #print(b.l)
+        window.ontimer(size,1)
+    else:
+        print('size zero')
 def boost_over():
-    
+    ball.pensize(0)
+    window.ontimer(ball.clear, 2000)
     ball.showturtle()
-    ball.clear()
     ball.penup()
-def boost_again():
-    ball.hide_status = 'no'
+    ball.clear()
+    b.l = 10
+    window.ontimer(time_over,10000)
+    size()
+
+    
+    
 def boost():
-    if ball.hide_status != 'yes':
+    if ball.status == 'no':
+        ball.status = 'yes'
         ball.hideturtle()
-        ball.pencolor('#fff')
         ball.pendown()
-        ball.hide_status = 'yes'
-    window.ontimer(boost_over,5000)
-    window.ontimer(boost_again,15000)
+        window.ontimer(boost_over,5000)
+    else:
+        write("can't use, boost on timer")
+
+        
+# msg1 = 'hello'
+# ball.write(msg1, align="center", font=("Mono Space",14,"bold"))
+# window.ontimer(ball.clear,1000)
+
+    
+
+
+    
+    
+
+
+
+        
 
 def over(x,y):
-    ball.a = False
+    if ball.invinsible != 'yes':
+        ball.a = False
+        exit()
+    else:
+        write('The ball is now invinsible')
         
 
 #keys
@@ -65,11 +169,38 @@ window.onkeypress(boost,'l')
 ball.onclick(over)
 
 
-
-
-
+window.ontimer(time_over,5000)
+###############################################################################################################################
+def position():
+    print(ball.xcor(),ball.ycor())
+    window.ontimer(position,1000)
+timer = 0
+#position()
+################################################################################################################################
 while ball.a:
+#################################################################################################################################
+    if seed.eaten == 'yes':
+        if ball.direction != 'stop':
+            timer += 10
+        timer += 1
+        if timer > 600:
+            if timer > random.randint(600,750):
+                timer = 0
+                summon_seed()
 
+    else:
+        pass
+        seed_collosion()
+    print(timer)
+#################################################################################################################################
+    
+    b.shapesize(b.l,b.w)
+    #print(ball.status)
+    if ball.status == 'no':
+        b.l = 4
+        b.color('green')
+    if ball.status == 'yes':
+        b.color('red')
 
 
 
