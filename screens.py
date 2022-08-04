d5ff47b8be4738a5d8d71854bb8d7f0386b92e15
Sumit <90320919+Sumitcoders@users.noreply.github.com>
2022-08-04 17:36:04 +0530

    release 1.2
    new instructions button on the main title screen

--- screens.py
+++ screens.py
@@ -1,10 +1,66 @@
-from re import S
 import turtle
 
 
 window = turtle
 window.addshape('ball.gif')
 window.addshape('cursor.gif')
+window.addshape('images/move.gif')
+
+
+
+def instruction():
+    window.tracer(0)
+    screen = turtle.Screen()
+    screen.setup(600,400)
+    screen.bgcolor('#fff')
+    txt = window.Turtle()
+    txt.penup()
+    txt.hideturtle()
+    #txt.goto(-90,150)
+    def writer(msg,x,y):
+        txt.goto(x,y)
+        txt.write(msg,align='center',font=('Mono space', 20, 'italic'))
+    """txt.write('''Move with w,a,s,d
+    or arrow key
+    ''',align='center',font=('Mono space', 20, 'italic'))"""
+    writer('''Move with w,a,s,d
+    or arrow key''',-90,150)
+
+    # instructions
+    window.addshape('images/g.gif')
+    move = window.Turtle()
+    move.shape('images/move.gif')
+    move.penup()
+    move.goto(-350,200)
+    g = window.Turtle()
+    g.shape('images/g.gif')
+    g.penup()
+    g.goto(-120,-100)
+    writer('''Watch that bar to 
+    turn green''',-380,-150)
+
+    window.addshape('images/hide.gif')
+    hide = window.Turtle()
+    hide.penup()
+    hide.shape('images/hide.gif')
+    hide.goto(500,-50)
+    writer('''Use the ablity to hide
+    from the cursor''',220,-50)
+
+    window.addshape('images/seed.gif')
+    seed = window.Turtle()
+    seed.shape('images/seed.gif')
+    seed.penup()
+    seed.goto(400,250)
+    writer('''Eat that seed to 
+    get invinsible''',140,250)
+
+
+    while True:
+        
+
+        window.update()
+        
 def title_screen():
     screen = window.Screen()
     screen.bgcolor('black')
@@ -33,17 +89,21 @@ def title_screen():
             ball.direction = 'down'
         else:
             ball.direction = 'right'
+            print('this is the prob')
             cursor.goto(-100,100)
             cursor.showturtle()
             cursor.speed(1)
             cursor.goto(-360,-7)
             ball.hideturtle()
             cursor.hideturtle()
+
             
         ball.turn += 1
         if ball.turn == 5:
             ball.turn = 1
-        window.ontimer(direction, 3000)
+            
+        if ball.a == True:
+            window.ontimer(direction, 3000)
     def game(x,y):
         print('ok')
         ball.a = False
@@ -59,11 +119,18 @@ def title_screen():
     txt.write('Wack the ball',align='center',font=('Mono space', 40, 'italic'))
     txt.goto(0,-100)
     txt.write('Click anywhere to start',align='center',font=('Mono space',10,'italic'))
-
-    
+    window.addshape('images/instructions.gif')
+    def click_i(x,y):
+        screen.clear()
+        instruction()
+    instruction_s = window.Turtle()
+    instruction_s.shape('images/instructions.gif')
+    instruction_s.penup()
+    instruction_s.goto(0,-200)
 
     window.listen()
     screen.onclick(game)
+    instruction_s.onclick(click_i)
     while ball.a:
         if ball.direction == 'up':
             ball.goto(ball.xcor(),ball.ycor()+ball.speed)
@@ -75,4 +142,3 @@ def title_screen():
             ball.goto(ball.xcor()+ball.speed,ball.ycor())
         print(ball.xcor(),ball.ycor())
         window.update()
-
